03832de37fafb7639b58e481d49b88c0d41a1d77
MCaliJr <MartinK.MoC@gmail.com>
2022-02-02 20:36:35 +0100

    styled info editing dashboard according to Navro's design

--- src/components/InfoEditingDashboard.jsx
+++ src/components/InfoEditingDashboard.jsx
@@ -19,43 +19,89 @@ const InfoEditingDashboard = () => {
   return (
     <div className="dashboard">
       <div className="dashboard__content">
-        <h3>Edit your page</h3>
-        <button className="dashboard__content__button-change-photo" type="button">
+        <h3 className="dashboard__content__h3">Edit your page</h3>
+        <button className="dashboard__content__button dashboard__content__button-change--photo" type="button">
           Change photo
         </button>
-        <p className="caption">Recommended dimension 256px x 256px. Max 2MB (jpg ,png).</p>
+        <p className="dashboard__content__p dashboard__content__p--photo-dimensions">
+          Recommended dimension 256px x 256px. Max 2MB (jpg ,png).
+        </p>
 
-        <h5>Personal data</h5>
+        <h5 className="dashboard__content__h5 dashboard__content__h5--section-divider">Personal data</h5>
 
-        <p className="caption">Name</p>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <p className="dashboard__content__p dashboard__content__p--input-names margin-top-0">Name</p>
+        <input
+          className="dashboard__content__input"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <p className="caption">Profession</p>
-        <input type="text" value={profession} onChange={(e) => setProfession(e.target.value)} />
+        <p className="dashboard__content__p dashboard__content__p--input-names">Profession</p>
+        <input
+          className="dashboard__content__input"
+          type="text"
+          value={profession}
+          onChange={(e) => setProfession(e.target.value)}
+        />
 
-        <p className="caption">Location</p>
-        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+        <p className="dashboard__content__p dashboard__content__p--input-names">Location</p>
+        <input
+          className="dashboard__content__input"
+          type="text"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
 
-        <p className="caption">Description</p>
-        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+        <p className="dashboard__content__p dashboard__content__p--input-names">Description</p>
+        <textarea
+          className="dashboard__content__input dashboard__content__input--text-area"
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-        <p className="caption">enter Your email</p>
-        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <p className="dashboard__content__p dashboard__content__p--input-names">enter Your email</p>
+        <input
+          className="dashboard__content__input"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <h5>Social media</h5>
-        <input type="text" value={twitterLink} onChange={(e) => setTwitterLink(e.target.value)} />
+        <h5 className="dashboard__content__h5 dashboard__content__h5--section-divider">Social media</h5>
 
-        <input type="text" value={linkedInLink} onChange={(e) => setLinkedInLink(e.target.value)} />
+        <input
+          className="dashboard__content__input dashboard__content__input--social-media"
+          type="text"
+          value={twitterLink}
+          onChange={(e) => setTwitterLink(e.target.value)}
+        />
 
-        <input type="text" value={youTubeLink} onChange={(e) => setYouTubeLink(e.target.value)} />
+        <input
+          className="dashboard__content__input dashboard__content__input--social-media"
+          type="text"
+          value={linkedInLink}
+          onChange={(e) => setLinkedInLink(e.target.value)}
+        />
 
-        <input type="text" value={gitHubLink} onChange={(e) => setGitHubLink(e.target.value)} />
+        <input
+          className="dashboard__content__input dashboard__content__input--social-media"
+          type="text"
+          value={youTubeLink}
+          onChange={(e) => setYouTubeLink(e.target.value)}
+        />
 
-        <div>
-          <button className="dashboard__content__button-save-changes" type="button">
-            Save Changes
-          </button>
-        </div>
+        <input
+          className="dashboard__content__input dashboard__content__input--social-media"
+          type="text"
+          value={gitHubLink}
+          onChange={(e) => setGitHubLink(e.target.value)}
+        />
+
+        <button className="dashboard__content__button dashboard__content__button-save-changes" type="button">
+          Save Changes
+        </button>
       </div>
     </div>
   );
