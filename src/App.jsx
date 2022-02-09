dac32a0e86d54661a059749c23e7bc478f3c58d2
MCaliJr <MartinK.MoC@gmail.com>
2022-02-09 20:21:46 +0100

    Page with live editing

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import LogIn from './pages/LogIn';
 import PublicPage from './pages/PublicPage';
 import SignUp from './pages/SignUp';
+import TestBuild from './pages/TestBuild';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/log-in" element={<LogIn />} />
         <Route path="/dashboard-view" element={<DashboardView />} />
+        <Route path="/test-build" element={<TestBuild />} />
       </Routes>
     </div>
   );
---
+++ src/pages/PublicPage_TestBuild.jsx
@@ -0,0 +1,63 @@
+import '../styles/PublicPage.css';
+import '../styles/Button.css';
+
+import data from '../../data/data.json';
+import Icon from '../components/Icon';
+
+const PublicPage = ({
+  name,
+  profession,
+  location,
+  description,
+  email,
+  twitterLink,
+  linkedInLink,
+  youTubeLink,
+  gitHubLink,
+}) => {
+  const { body, photo, theme, socials, signature } = data;
+
+  const experienceList = body.experience.map((work) => {
+    return (
+      <li key={work.id}>
+        <p>{work.text}</p>
+      </li>
+    );
+  });
+
+  const socialList = socials.map((social) => {
+    return (
+      <li key={social.id}>
+        <a href={social.link} target="_blank" rel="noreferrer">
+          <Icon name={`${social.id}.svg`} />
+        </a>
+      </li>
+    );
+  });
+
+  return (
+    <div className="public">
+      <div className="public__img">
+        <img src={photo.src} alt={name} />
+      </div>
+      <div className="public__content">
+        <h2>{name}</h2>
+        <h5>
+          {profession} from {location}
+        </h5>
+        <p className="description">{description}</p>
+      </div>
+      <button type="button" className="btn" onClick={() => alert(`Email sent to: ${email}`)}>
+        Contact me
+      </button>
+      <div className="public__socials">
+        <ul className="public__socials-list">{socialList}</ul>
+      </div>
+      <div className="public__signature">
+        <img src="/public/signature.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default PublicPage;
