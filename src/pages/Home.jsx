b45dde3fb9045d2e009fe1b6dba1307e20f02ac5
Dawid Gaweł <dmgawel@users.noreply.github.com>
2022-02-10 17:50:41 +0100

    add background gradient effect (#27)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,37 @@
 import '../styles/Home.css';
 
-import frame from '../../public/frame.png';
-import Button from '../components/Button';
+import React from 'react';
 
-const Home = () => {
-  return (
-    <div className="home">
-      <section className="home__section">
-        <div className="home__text">
-          <h1>Build your portfolio website</h1>
-          <h5>One page in less than 5 minutes!</h5>
-          <Button pathname="/try-it-out" text="Build up" />
+import frame from '../../public/frame.png';
+
+import Button from '../components/Button';
+import { Gradient } from '../vendor/Gradient.js';
+
+class Home extends React.Component {
+  componentDidMount() {
+    // Create your instance
+    const gradient = new Gradient();
+
+    // Call `initGradient` with the selector to your canvas
+    gradient.initGradient('#gradient-canvas');
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="home__canvas">
+          <canvas id="gradient-canvas" data-transition-in />
         </div>
-        <div className="home__img">
-          <img src={frame} className="home__frame" alt="showcase-img" />
-        </div>
+        <section className="home__section">
+          <div className="home__text">
+            <h1>Build your portfolio website</h1>
+            <h5>One page in less than 5 minutes!</h5>
+            <Button pathname="/try-it-out" text="Build up" />
+          </div>
+          <div className="home__img">
+            <img src={frame} className="home__frame" alt="showcase-img" />
+          </div>
+        </section>
         <div className="home__wave">
           <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path
@@ -23,9 +40,9 @@ const Home = () => {
             />
           </svg>
         </div>
-      </section>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default Home;
