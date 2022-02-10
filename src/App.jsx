e5ce6a1e75fef9c2ec133c5bdacce800afac633e
Mateusz Obłoza <95107037+zyzgz@users.noreply.github.com>
2022-02-10 11:38:07 +0100

    routing for register and login (#25)

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import DashboardView from './pages/DashboardView';
 import Home from './pages/Home';
-import LogIn from './pages/LogIn';
+import Login from './pages/login/LoginLogic';
 import PublicPage from './pages/PublicPage';
-import SignUp from './pages/SignUp';
+import SignupForm from './pages/Register/SignupForm';
 
 const App = () => {
   return (
@@ -16,8 +16,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/public" element={<PublicPage />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+        <Route path="/log-in" element={<Login />} />
         <Route path="/dashboard-view" element={<DashboardView />} />
       </Routes>
     </div>
