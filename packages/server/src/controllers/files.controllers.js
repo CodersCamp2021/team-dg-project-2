014e8f98dacf3e4d855e7ea843a3f6263dbf1213
MCaliJr <MartinK.MoC@gmail.com>
2022-03-21 17:30:48 +0100

    Add GET /api/files/:id route

--- packages/server/src/controllers/files.controllers.js
+++ packages/server/src/controllers/files.controllers.js
@@ -15,6 +15,20 @@ const filesControllers = (router) => {
     res.status(200).send(file);
   });
 
+  // @desc Get file by Id from parameter
+  // @route GET /api/files/:id
+  // @access Public
+  router.get('/files/:id', async (_req, res) => {
+    const file = await File.findById(_req.params.id);
+
+    if (!file) {
+      res.status(400);
+      throw new Error('File not found');
+    }
+
+    res.status(200).send(file.filePath);
+  });
+
   // Use fileUpload middleware for methods below this line
   router.use(
     fileUpload({
@@ -25,7 +39,7 @@ const filesControllers = (router) => {
   // @desc upload file to server
   // @route POST /api/files
   // @access Private
-  router.post('/files', async function (req, res) {
+  router.post('/files', async (req, res) => {
     // check if file was attached to the form
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
@@ -42,12 +56,12 @@ const filesControllers = (router) => {
 
     // create info about the file in DB
     await File.create({
-      filePath: uploadPath,
+      filePath: `/public/${sampleFile.name}`,
       name: sampleFile.name,
     });
 
     // mv() let us place the file somewhere on server
-    sampleFile.mv(uploadPath, function (err) {
+    sampleFile.mv(uploadPath, (err) => {
       if (err) return res.status(500).send(err);
 
       res.send('File uploaded!');
