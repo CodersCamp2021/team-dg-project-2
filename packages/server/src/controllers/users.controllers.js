b10ec877daa02b23c826ce31ab762cdd7e1d9b30
MCaliJr <MartinK.MoC@gmail.com>
2022-03-24 13:13:46 +0100

    Update users controlers to match documentation (almost - we need to change /users/login to POST from GET

--- packages/server/src/controllers/users.controllers.js
+++ packages/server/src/controllers/users.controllers.js
@@ -7,6 +7,7 @@ import User from '../models/userSchema';
 const usersControllers = (router) => {
   // @desc Register user
   // @route POST /api/users
+  // @access Public
   router.post(
     '/users',
     asyncHandler(async (req, res) => {
@@ -53,18 +54,20 @@ const usersControllers = (router) => {
         res.status(StatusCodes.CREATED).send({
           _id: user.id,
           email: user.email,
+          password: hashedPassword,
           slug: user.slug,
         });
       } else {
         res.status(StatusCodes.BAD_REQUEST).send({ error: 'invalid user data' });
       }
-
-      res.json({ message: 'register user' });
+      // This code leads to error, so I am commenting it out
+      // res.json({ message: 'register user' });
     })
   );
 
   // @desc Login user
-  // @route POST /api/users/me
+  // @route POST /api/users/login
+  // @access Public
   router.post(
     '/users/login',
     asyncHandler(async (req, res) => {
@@ -89,10 +92,72 @@ const usersControllers = (router) => {
         res.json({
           _id: user.id,
           email: user.email,
+          // TODO - add token: TOKEN
         });
       } else {
         res.status(StatusCodes.BAD_REQUEST).send({ error: 'Invalid credentials' });
       }
+    })
+  );
+
+  // @desc update user account info
+  // @route PUT /api/users/:id
+  // @access Private
+  router.put(
+    '/users/:id',
+    asyncHandler(async (req, res) => {
+      const { email, password, token } = req.body;
+
+      const { id } = req.params;
+
+      //  Check if user of that id exists
+      const userExists = await User.findById({ _id: req.params.id });
+
+      if (!userExists) {
+        res.status(StatusCodes.BAD_REQUEST).send({ error: 'This user does not exist' });
+        return;
+      }
+
+      //  Hash password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      // Update user info
+      await User.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          email: req.body.email,
+          password: hashedPassword,
+          // TODO token
+        },
+        { new: true }
+      );
+
+      res.send({ message: `Below is your updated info`, email: req.body.email, password: "It's updated, trust us ;)" });
+    })
+  );
+
+  // @desc delete user account info
+  // @route DELETE /api/users/:id
+  // @access Private
+  router.delete(
+    '/users/:id',
+    asyncHandler(async (req, res) => {
+      const { email, password, token } = req.body;
+
+      // TODO check for token!!!
+
+      //  Check if user of that id exists
+      const user = await User.findById({ _id: req.params.id });
+
+      if (!user) {
+        res.status(StatusCodes.BAD_REQUEST).send({ error: 'This user does not exist' });
+        return;
+      }
+
+      await user.remove();
+
+      res.send({ message: `User with email: ${email} was removed` });
     })
   );
 };
